14f3795e7dc100017b28b7137b49eef003d53d78
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2021-01-09 23:47:13 +0300

    Add project files.

---
+++ HasanFurkanFidanBlogService/Entities/Article.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HasanFurkanFidanBlogService.Entities
+{
+    public class Article
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ContentSummary { get; set; }
+        public string MainContent { get; set; }
+        public string ImageUrl { get; set; }
+        public int ViewCount { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
